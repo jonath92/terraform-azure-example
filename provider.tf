81634c2129dc8b4de90040b401c3be40d0a689d2
jonathan Sarrazin <jonathan.sarrazin@prodyna.com>
2024-07-01 13:59:02 +0200

    add remore state

--- provider.tf
+++ provider.tf
@@ -4,10 +4,18 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "~>3.0"
     }
+
     random = {
       source  = "hashicorp/random"
       version = "~>3.0"
     }
+  }
+
+  backend "azurerm" {
+    resource_group_name  = "tfstate-github"
+    storage_account_name = "tfstate23165"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
   }
 }
 
