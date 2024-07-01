58dbf2596fb6ea022fcf2de4421cdfda85ff85e0
jonathan Sarrazin <jonathan.sarrazin@prodyna.com>
2024-07-01 14:39:07 +0200

    add use oidc

--- provider.tf
+++ provider.tf
@@ -16,6 +16,7 @@ terraform {
     storage_account_name = "tfstate23165"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
+    use_oidc             = true
   }
 }
 
